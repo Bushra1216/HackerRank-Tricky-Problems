b34b99120efbd36ec826d8bf5ef89088e9e04cdb
Shanjida Jahan Bushra <156702727+Bushra1216@users.noreply.github.com>
2024-10-13 00:44:43 +0600

    The report - Students grade and marks.sql

--- The report - Students grade with marks.sql
+++ The report - Students grade with marks.sql
@@ -1,8 +1,8 @@
 ---The Report problem
-select
-case
-when Grades.Grade<8 then 'NULL'
-else Students.Name
-END AS tb, Grades.Grade as gd,Students.Marks as mk
-from Students INNER JOIN Grades ON Students.Marks between  Grades.Min_Mark and Grades.Max_Mark 
-order by Grades.Grade desc,Students.Name asc,Students.Marks asc;
+SELECT
+CASE
+WHEN Grades.Grade<8 THEN 'NULL'
+ELSE Students.Name
+END AS tb, Grades.Grade, Students.Marks
+FROM Students INNER JOIN Grades ON Students.Marks BETWEEN Grades.Min_Mark AND Grades.Max_Mark 
+ORDER BY Grades.Grade DESC, Students.Name ASC,Students.Marks ASC;
